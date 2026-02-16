c7d6d18dad55d1503080693c18ea96b2edd11d2d
Dasari-Deepthi05 <22r21a0580@mlrit.ac.in>
2026-02-16 17:15:14 +0530

    Update settings.py

--- attrition/settings.py
+++ attrition/settings.py
@@ -92,7 +92,14 @@ WSGI_APPLICATION = 'attrition.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
+    }
 }
 
 
